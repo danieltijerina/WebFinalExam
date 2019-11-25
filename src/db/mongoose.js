45581d23a40e75fbf7e5306879ced2150078c964
danieltijerina <dtg.danieltijerina@gmail.com>
2019-11-25 17:07:38 -0600

    Initial commit.

---
+++ src/db/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+// revisa tu connectionURL aqui :-)
+if (process.env.NODE_ENV == 'production') {
+  var connectionURL = process.env.connectionURL;
+} else {
+  const config = require('../config');
+  var connectionURL = config.connectionURL;
+}
+
+mongoose.connect( connectionURL, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+})
